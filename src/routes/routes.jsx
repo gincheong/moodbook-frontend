5e4bdc5cebcaf850cb72e223ac95f9b8e7f672a4
gincheong2 <gincheong2@gmail.com>
2025-07-11 23:54:09 +0900

    feature(route): 라우트 Layout별로 분리

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,18 +3,24 @@ import { Main } from '@/pages/Main/Main';
 import { Book } from '@/pages/Book/Book';
 import { MyPage } from '@/pages/MyPage/MyPage';
 import { Admin } from '@/pages/Admin/Admin';
+import { SignUp } from '@/pages/SignUp/SignUp';
 import { MainLayout } from '@/layout/MainLayout/MainLayout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: MainLayout,
     children: [
       // * 아래 항목들의 `Component`에 해당되는 부분이 MainLayout에 있는 <Outlet />에 들어갑니다.
-      { index: true, Component: Main },
-      { path: 'book', Component: Book },
-      { path: 'me', Component: MyPage },
-      { path: 'admin', Component: Admin },
+      {
+        Component: MainLayout,
+        children: [
+          { index: true, Component: Main },
+          { path: 'book', Component: Book },
+          { path: 'me', Component: MyPage },
+          { path: 'admin', Component: Admin },
+        ],
+      },
+      { path: 'login', Component: SignUp },
     ],
   },
 ]);
