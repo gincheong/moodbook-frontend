868490e17f2183d1dffff4ccabe5c992d279ae51
gincheong2 <gincheong2@gmail.com>
2025-07-11 23:54:54 +0900

    feature(util): 인증정보 저장용 localStorage 유틸 추가

---
+++ src/utils/storage.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} key localStorage Key
+ */
+export const getLocalStorageItem = (key) => {
+  const value = localStorage.getItem(key);
+  if (value) {
+    return JSON.parse(value);
+  }
+  return value;
+};
+
+/**
+ * @param {string} key localStorage Key
+ * @param {any} value localStorage value
+ */
+export const setLocalStorageItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
