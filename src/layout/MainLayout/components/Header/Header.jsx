0123d9183a2491ca599689913c86a1049d0e5ac1
gincheong2 <gincheong2@gmail.com>
2025-07-13 16:21:06 +0900

    feat(header): 헤더 영역 antd 적용, 프로필 버튼 드롭다운 적용

--- src/layout/MainLayout/components/Header/Header.jsx
+++ src/layout/MainLayout/components/Header/Header.jsx
@@ -1,19 +1,30 @@
 import styles from './Header.module.css';
 import profileImage from '@/assets/profile.png';
+import { Dropdown, Input } from 'antd';
 import { Link } from 'react-router';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.inputWrapper}>
-        <input className={styles.input} type='text' placeholder='검색어를 입력하세요' />
+        <Input type='text' placeholder='검색어를 입력하세요.' />
       </div>
-      <Link to='/admin' className={styles.adminPageButton}>
-        관리자 페이지
-      </Link>
-      <Link to='/me' className={styles.profileIconButton}>
+      <Dropdown
+        menu={{
+          items: [
+            {
+              key: 'admin',
+              label: <Link to='/me'>마이페이지</Link>,
+            },
+            {
+              key: 'me',
+              label: <Link to='/admin'>관리자 페이지</Link>,
+            },
+          ],
+        }}
+      >
         <img className={styles.profileImage} src={profileImage} alt='profile image' />
-      </Link>
+      </Dropdown>
     </header>
   );
 };
