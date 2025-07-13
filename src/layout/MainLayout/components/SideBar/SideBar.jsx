15b6fa877f4b16241afbbf411f422e0025702dbc
gincheong2 <gincheong2@gmail.com>
2025-07-13 16:18:14 +0900

    feat(layout): 사이드바 antd 적용

--- src/layout/MainLayout/components/SideBar/SideBar.jsx
+++ src/layout/MainLayout/components/SideBar/SideBar.jsx
@@ -1,14 +1,15 @@
+import { Layout } from 'antd';
 import styles from './SideBar.module.css';
 import moodBookLogo from '@/assets/moodbook_logo.png';
 
 export const SideBar = () => {
   return (
-    <section className={styles.sideBar}>
+    <Layout.Sider className={styles.sidebar} breakpoint='md' width={200}>
       <img className={styles.logo} src={moodBookLogo} alt='MoodBook Logo' />
       <div className={styles.description}>
         <span>{'지금 내가 읽고 싶은 책,\nAI가 찾아드릴게요'}</span>
         <span>오늘의 기분은 어떠신가요?</span>
       </div>
-    </section>
+    </Layout.Sider>
   );
 };
