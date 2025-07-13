3528d29f8ac765b4451857ebccfbd7efb909c0f4
gincheong2 <gincheong2@gmail.com>
2025-07-13 16:34:10 +0900

    feat(sidebar): 사이드바 레이아웃 작성

--- src/layout/MainLayout/components/SideBar/SideBar.jsx
+++ src/layout/MainLayout/components/SideBar/SideBar.jsx
@@ -1,17 +1,20 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
 import styles from './SideBar.module.css';
 import moodBookLogo from '@/assets/moodbook_logo.png';
 import { Link } from 'react-router';
 
 export const SideBar = () => {
   return (
-    <Layout.Sider className={styles.sidebar} breakpoint='md' width={200}>
-      <Link to='/' className={styles.logoAnchor}>
-        <img className={styles.logo} src={moodBookLogo} alt='MoodBook Logo' />
-      </Link>
-      <div className={styles.description}>
-        <span>{'지금 내가 읽고 싶은 책,\nAI가 찾아드릴게요'}</span>
-        <span>오늘의 기분은 어떠신가요?</span>
+    <Layout.Sider breakpoint='md' width={200}>
+      <h1 className={styles.title}>MoodBook</h1>
+      <div className={styles.content}>
+        <Link to='/' className={styles.logoAnchor}>
+          <img className={styles.logo} src={moodBookLogo} alt='MoodBook Logo' />
+        </Link>
+        <div className={styles.description}>
+          <span>{'지금 내가 읽고 싶은 책,\nAI가 찾아드릴게요'}</span>
+          <span>오늘의 기분은 어떠신가요?</span>
+        </div>
       </div>
     </Layout.Sider>
   );
