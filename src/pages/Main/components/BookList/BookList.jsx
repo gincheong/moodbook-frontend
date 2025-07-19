ace773e97a460ccfd3620b26676daaa1df772d1a
gincheong2 <gincheong2@gmail.com>
2025-07-19 18:59:28 +0900

    feat: 책 표시 레이아웃 변경

--- src/pages/Main/components/BookList/BookList.jsx
+++ src/pages/Main/components/BookList/BookList.jsx
@@ -1,26 +1,15 @@
 import { Card, Image, Typography } from 'antd';
 import styles from './BookList.module.css';
-import { useRef } from 'react';
-import { useIntersectionObserver } from '../../../../hooks/useIntersectionObserver';
+import { CardContent, Cover, Description, Title } from './BookList.styles';
 
 /**
  * @param {object} props
  * @param {object[]} props.books
  * @param {string} props.title
- * @param {function} [props.onIntersect]
  */
 export const BookList = (props) => {
   // TODO Carousel 적용
-  const { title, books, onIntersect } = props;
-
-  const infinityScrollIndicatorRef = useRef(null);
-
-  useIntersectionObserver({
-    target: infinityScrollIndicatorRef.current,
-    onIntersect: () => {
-      onIntersect && onIntersect();
-    },
-  });
+  const { title, books } = props;
 
   return (
     <section>
@@ -28,15 +17,23 @@ export const BookList = (props) => {
       <li className={styles.list}>
         {books.map((book, index) => (
           <ol key={`${book.id}-${index}`} className={styles.item}>
-            <Card title={book.title} size='small'>
-              <Image src={book.coverImageUrl} alt={`image-alt-${book.title}`} />
+            <Card size='small'>
+              <CardContent>
+                <div className={styles.text}>
+                  <Title>{book.title}</Title>
+                  <Description>{book.description}</Description>
+                </div>
+                <Cover>
+                  <Image
+                    width={100}
+                    src={book.coverImage}
+                    alt={`image-alt-${book.title}`}
+                  />
+                </Cover>
+              </CardContent>
             </Card>
           </ol>
         ))}
-        <div
-          ref={infinityScrollIndicatorRef}
-          className={styles.infinityScrollIndicator}
-        />
       </li>
     </section>
   );
---
+++ src/pages/Main/components/BookList/BookList.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
+  padding: 8px;
+  width: 180px;
+`;
+
+export const Title = styled.span`
+  display: flex;
+  flex-direction: column;
+  row-gap: 2px;
+`;
+
+export const Description = styled.span`
+  font-size: 10px;
+  color: #727272;
+`;
+
+export const Cover = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
