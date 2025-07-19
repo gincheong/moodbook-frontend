51567f46ded72d6d15c10da411c976b4f97d2541
gincheong2 <gincheong2@gmail.com>
2025-07-19 17:58:06 +0900

    refactor: 변수명 변경

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,9 +1,9 @@
 /**
- * @typedef {Object} EnvType
+ * @typedef {Object} Env
  * @property {string} VITE_API_ENDPOINT
  */
 
-/** @type {EnvType} */
-export const Env = {
+/** @type {Env} */
+export const Envs = {
   VITE_API_ENDPOINT: import.meta.env.VITE_API_BASE_URL,
 };
