26ab8dba6eca2cada8450ef32988619ba72f4954
gincheong2 <gincheong2@gmail.com>
2025-07-26 09:19:47 +0900

    feat: 미팅 화면 파일들 추가, 401시 로그인 화면으로 리다이렉트

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,16 +1,22 @@
 import { getLocalStorageItem, StorageKeys } from '@/utils/storage';
 
-export const defaultFetch = (
+export const defaultFetch = async (
   input: string | URL | globalThis.Request,
   init: RequestInit,
 ) => {
   const accessToken = getLocalStorageItem(StorageKeys.ACCESS_TOKEN);
 
-  return fetch(input, {
+  const response = await fetch(input, {
     ...init,
     headers: {
       Authorization: `Bearer ${accessToken}`,
       'Content-type': 'application/json',
     },
   });
+
+  if (response.status === 401) {
+    location.href = '/sign-in';
+  }
+
+  return response;
 };
