add0ccebe9b62c79a54c9e94865d543541a4a73f
gincheong2 <gincheong2@gmail.com>
2025-07-26 11:24:19 +0900

    wip: 도서 검색 페이지

--- src/apis/books.ts
+++ src/apis/books.ts
@@ -91,3 +91,14 @@ export const requestGetBookMarks = () => {
 
   return defaultFetch(url, { method: 'GET' });
 };
+
+export type RequestGetBooksWithKeywordResponse = PageContent<Book>;
+/**
+ * 전체 도서 조회
+ * @return RequestGetBooksResponse
+ */
+export const requestGetBooksWithKeyword = (keyword: string) => {
+  const url = `${Envs.VITE_API_ENDPOINT}/api/books/autocomplete?keyword=${keyword}`;
+
+  return defaultFetch(url, { method: 'GET' });
+};
