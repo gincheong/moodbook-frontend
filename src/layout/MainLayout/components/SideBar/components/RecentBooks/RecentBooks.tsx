89e55b937e20bd632bb663ec53be3a4902d2f1fc
gincheong2 <gincheong2@gmail.com>
2025-07-26 10:13:45 +0900

    feat: 최근 도서 클릭하여 상세 페이지로 이동

--- src/layout/MainLayout/components/SideBar/components/RecentBooks/RecentBooks.tsx
+++ src/layout/MainLayout/components/SideBar/components/RecentBooks/RecentBooks.tsx
@@ -6,16 +6,27 @@ import {
   RecentBookImg,
   TextStyles,
 } from './RecentBooks.styles.ts';
+import { useNavigate } from 'react-router';
+import { Paths } from '@/routes/routes.ts';
 
 export const RecentBooks = () => {
   const { recentBooks } = useRecentBooks();
+  const navigate = useNavigate();
+
+  const onBookImgClick = (bookId: number) => {
+    navigate(`${Paths.BOOK}?id=${bookId}`);
+  };
 
   return (
     <Container>
       <Typography.Text style={TextStyles}>최근 본 도서</Typography.Text>
       <Books>
         {recentBooks.map((book) => (
-          <RecentBookImg key={book.bookId} src={book.coverImage} />
+          <RecentBookImg
+            key={book.bookId}
+            src={book.coverImage}
+            onClick={() => onBookImgClick(book.bookId)}
+          />
         ))}
       </Books>
     </Container>
